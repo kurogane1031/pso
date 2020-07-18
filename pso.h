1a943c5c5a2ed023cef4da2d59edbbfe16c4ecce
Muhammad Zulfaqar bin Azmi <mzulfaqar88@gmail.com>
2020-07-19 08:03:20 +0900

    std::transform

--- pso.h
+++ pso.h
@@ -2,10 +2,12 @@
 #include <cassert>
 #include <cmath>
 #include <ctime>
-#include <iostream>
 #include <random>
 #include <vector>
 #include <functional>
+#include <algorithm>
+#include <cstdlib>
+#include <fmt/core.h>
 
 namespace plt=matplotlibcpp;
 const float inf = 100000000.f;
@@ -29,6 +31,22 @@ T sphere(const std::vector<T> &vec) {
   }
   return temp;
 }
+
+template <class T>
+T schwefel(const std::vector<T> &vec){
+  T temp_i = 0.0f;
+  for(size_t i = 0; i < vec.size(); ++i){
+    T temp_j = 0.0f;
+    for(size_t j = 0; j < (i + 1); j++){
+      temp_j += vec.at(j);
+    }
+    temp_i += temp_j * temp_j;
+  }
+  return temp_i;
+}
+
+template<class T>
+void print_sth();
 
 template <class T>
 T table(const std::vector<T> &vec) {
@@ -65,6 +83,11 @@ struct Swarm{
     best_global.fitness = inf;
 
   }
+
+  Swarm(const Swarm &swarm)
+      : best_global(swarm.best_global), particles(swarm.particles){
+    std::cout << "Copied!\n";
+  }
 }; // end Swarm type
 
 template <class T>
@@ -90,29 +113,32 @@ private:
   // ###################
   //  Initialize default
   Swarm<T> swarm = Swarm<T>(n_var, number_of_particle);
-
  public:
-  PSO(const int& n_particle, const int& max_iter, const T& min_w, const T& max_w,
-      const T& c1, const T& c2, const int& n_var,
-      const std::vector<T>& lb, const std::vector<T>& ub,
-      std::function<T(const std::vector<T>&)> f)
-      : number_of_particle(n_particle),
-        max_iter(max_iter),
-        min_inertia_weight(min_w),
-        max_inertia_weight(max_w),
-        c1(c1), c2(c2), n_var(n_var),
-        lower_bound(lb), upper_bound(ub){
-    
-    assert(swarm.particles.size() == number_of_particle);
-    // func = static_cast<T (*)(const std::vector<T> &)>(f);
-    func = f;
-    std::transform(lb.begin(), lb.end(), ub.begin(),
-                       std::back_inserter(min_velocity), get_min_velocity<T>);
-    std::transform(lb.begin(), lb.end(), ub.begin(),
-                   std::back_inserter(max_velocity), get_max_velocity<T>);
+   PSO(const int &n_particle, const int &max_iter, const T &min_w,
+       const T &max_w, const T &c1, const T &c2, const int &n_var,
+       const std::vector<T> &lb, const std::vector<T> &ub,
+       std::function<T(const std::vector<T> &)> f)
+       : n_var(n_var), lower_bound(lb), upper_bound(ub),
+         number_of_particle(n_particle), max_iter(max_iter),
+         min_inertia_weight(min_w), max_inertia_weight(max_w), c1(c1), c2(c2) {
+     // assert(swarm.particles.size() == number_of_particle);
+     // func = static_cast<T (*)(const std::vector<T> &)>(f);
+     func = f;
 
-    init(ub, lb);
-  }
+     std::transform(lb.begin(),
+                    lb.end(),
+                    ub.begin(),
+                    std::back_inserter(min_velocity),
+                    get_min_velocity<T>);
+
+     std::transform(lb.begin(),
+                    lb.end(),
+                    ub.begin(),
+                    std::back_inserter(max_velocity),
+                    get_max_velocity<T>);
+
+     init(ub, lb);
+   }
 
   // creates x number of uniformly distributed random
   const std::vector<T> random_float(){
@@ -132,19 +158,35 @@ private:
     for (auto it = swarm.particles.begin(); it != swarm.particles.end(); ++it){
       std::vector<T> rand = random_float();
       for(int iter = 0; iter < n_var; ++iter){
-        it->position[iter] = (ub[iter] - lb[iter]) * rand[iter] + lb[iter];
-        it->velocities[iter] = 0.0;
-        it->best_local.position[iter] = 0.0;
+        it->position.at(iter) = (ub.at(iter) - lb.at(iter)) * rand.at(iter) + lb.at(iter);
+        it->velocities.at(iter) = 0.0;
+        it->best_local.position.at(iter) = 0.0;
       }
     }
   }
 
-  auto personal_best(const T& C1, const T& R1, const T& P, const T& position){
-    return C1 * R1 * (P - position);
+  T update_velocity(const T& w, const T& v,
+                    const T& c1, const T& c2,
+                    const T& r1, const T& r2,
+                    const T& P, const T& G,
+                    const T& position){
+    return (w * v + c1 * r1 * (P - position) + c2 * r2 * (G - position));
   }
 
-  auto global_best(const T& C2, const T& R2, const T& G, const T& position){
-    return C2 * R2 * (G - position);
+  // void update_velocity(const T& weight,
+  //                      const T& c1,
+  //                      const T& c2,
+  //                      std::vector<T> velocity,
+  //                      const std::vector<T>& rand1,
+  //                      const std::vector<T>& rand2,
+  //                      const std::vector<T>& position,
+  //                      const std::vector<T>& global_best
+  //                      ){
+
+  // }
+
+  T update_position(const T& position, const T& velocity){
+    return position + velocity;
   }
 
   // main loop that starts the optimization and return a global best
@@ -174,39 +216,107 @@ private:
       // update inertia weight
       const T w = max_inertia_weight - weight_diff * curr_iter / max_iter;
 
+      T lbb = lower_bound.at(0);
+      T upp = upper_bound.at(0);
       // update exploration and exploitation
-        const std::vector<T> r1 = random_float();
-        const std::vector<T> r2 = random_float();
+      const std::vector<T> rand1 = random_float();
+      const std::vector<T> rand2 = random_float();
       for (auto it = swarm.particles.begin(); it != swarm.particles.end(); ++it){
-        for (int i = 0; i < n_var; i++) {
-          it->velocities[i] = w * it->velocities[i] + personal_best(c1, r1[i], it->best_local.position[i], it->position[i])
-                     + global_best(c2, r2[i], swarm.best_global.position[i], it->position[i]); // update velocity vector
 
-          if (it->velocities[i] < min_velocity[i]) {
-            it->velocities[i] = min_velocity[i];
-          }
-          if (it->velocities[i] > max_velocity[i]) {
-            it->velocities[i] = max_velocity[i];
-          }
-          it->position[i] = it->position[i] + it->velocities[i]; //update position vector
-          if(it->position[i] < lower_bound[i]){
-            it->position[i] = lower_bound[i];
-          }
-          if(it->position[i] > upper_bound[i]){
-            it->position[i] = upper_bound[i];
-          }
+        for (int i = 0; i < n_var; i++) {
+          it->velocities.at(i) = update_velocity(
+              w, it->velocities.at(i), c1, c2, rand1.at(i), rand2.at(i),
+              it->best_local.position.at(i), swarm.best_global.position.at(i),
+              it->position.at(i)); // update velocity vector
         }
+
+        // std::transform(it->velocities.begin(),
+        //                it->velocities.end(),
+        //                it->velocities.begin(),
+        //                [minn = std::cbegin(min_velocity)](auto vel) mutable {
+        //                  if(std::less<T>{}(vel, *minn)){
+        //                    return *minn++;
+        //                  }
+        //                  else{
+        //                    minn++;
+        //                    return vel;
+        //                  }
+        //                });
+
+        // std::transform(it->velocities.begin(), it->velocities.end(),
+        //                it->velocities.begin(),
+        //                [maxx = std::cbegin(max_velocity)](auto vel) mutable {
+        //                  if (std::greater<T>{}(vel, *maxx)) {
+        //                    return *maxx++;
+        //                  }
+        //                  else {
+        //                    maxx++;
+        //                    return vel;
+        //                  }
+        //                });
+
+        std::transform(it->velocities.begin(), it->velocities.end(),
+                       it->velocities.begin(),
+                       [minn = std::cbegin(min_velocity),
+                        maxx = std::cbegin(max_velocity)](auto vel) mutable {
+                         if (std::less<T>{}(vel, *minn)) {
+                           maxx++;
+                           return *minn++;
+                         } else if (std::greater<T>{}(vel, *maxx)) {
+                           minn++;
+                           return *maxx++;
+                         } else {
+                           minn++;
+                           maxx++;
+                           return vel;
+                         }
+                       });
+
+        // std::replace_if(it->velocities.begin(),
+        //                 it->velocities.end(),
+        //                 [&minn](const T& v){
+        //                   return v < minn;
+        //                 },
+        //                 minn);
+        
+        //update position
+        std::transform(it->position.begin(),
+                       it->position.end(),
+                       it->velocities.begin(),
+                       it->position.begin(),
+                       std::plus<T>());
+
+        std::replace_if(it->position.begin(),
+                        it->position.end(),
+                        [&lbb](const T &p){
+                          return p < lbb;
+                        },
+                        lbb);
+        
+        std::replace_if(it->position.begin(),
+                        it->position.end(),
+                        [&upp](const T &p){
+                          return p > upp;
+                        },
+                        upp);
       }
+
       track_gbest[curr_iter] = swarm.best_global.fitness;
       track_iter[curr_iter] = curr_iter;
-      std::cout << "Iteration# " << curr_iter + 1 <<"\t" << "G_Best_O = " << swarm.best_global.fitness << "\n";
+      std::system("clear");
+      
+      fmt::print("Iteration# {} \t Global Best O = {} \n", curr_iter+1, swarm.best_global.fitness);
+      fmt::print("Best global X = \n");
+
+      for(const auto& gX:swarm.best_global.position){
+        fmt::print("{}\n", gX);
+      }
     }
-    // plt::plot(track_gbest);
-    plt::semilogy(track_iter, track_gbest);
-    // plt::ylim(0, 100);
-    plt::xlabel("Iteration");
-    plt::ylabel("Global Best Objective");
-    plt::show();
+
+    // plt::semilogy(track_iter, track_gbest);
+    // plt::xlabel("Iteration");
+    // plt::ylabel("Global Best Objective");
+    // plt::show();
   }
 };
 
